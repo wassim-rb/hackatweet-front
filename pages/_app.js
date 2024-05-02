2aefed6209b7ea387b81688b4fbfa9ee13fe1eb2
Wassim.rb <rbilawassim@gmail.coml>
2024-05-02 09:55:08 +0200

    install packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,35 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({ });
+const persistConfig = { key: 'hackatweet-store', storage }
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+ });
+
+ const persistor = persistStore(store);
+
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store ={store}>
+      <PersistGate persistor={persistor}>
       <Head>
         <title>Next.js App</title>
       </Head>
       <Component {...pageProps} />
-    </>
+      </PersistGate>
+    </Provider>
   );
 }
 
