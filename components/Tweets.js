ce949261860140b9ad57e93e070ecac0be6b19fc
Your name <Your email>
2024-05-03 12:56:30 +0200

    nina frontend test push tweet whith connection merge via wassim

--- components/Tweets.js
+++ components/Tweets.js
@@ -1,32 +1,52 @@
 import React from "react";
 import { useState } from "react";
 import styles from "../styles/tweets.module.css";
+import { useSelector } from "react-redux";
 
 function Tweet() {
   const [counter, setCounter] = useState("");
+  const [addNewDescription, setaddNewDescription] = useState("");
+  const token = useSelector((state) => state.user.value);
+  const handleClick = () => {
+    // console.log(description);
+    console.log("click post");
+    fetch("http://localhost:3000/tweet/tweetUser", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        token: token,
+        description: addNewDescription,
+      }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setaddNewDescription("");
+      });
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.left}></div>
       <div className={styles.middle}>
         <div className={styles.headerMiddle}>
-          <p>HOME</p>
-          {/* <textarea
+          <p className={styles.homeheader}>HOME</p>
+          <textarea
             className={styles.textarea}
-            type="text"
-            name="description"
-            size="280"
-            required
-            //class="input"
-            placeholder="Enter the new post"
-          /> */}
-          <input
-            className={styles.textarea}
-            type="textarea"
-            maxlength="280"
+            // type="textarea"
+            cols="45"
+            maxLength="280"
             onChange={(e) => setCounter(e.target.value)}
             value={counter}
           />
-          {counter.length}
+          <div className={styles.counteretBouton}>
+            <div className={styles.counter}>{counter.length}/280</div>
+            <button
+              className={styles.boutonTweet}
+              onClick={() => handleClick()}
+            >
+              Tweet
+            </button>
+          </div>
         </div>
       </div>
       <div className={styles.right}>
